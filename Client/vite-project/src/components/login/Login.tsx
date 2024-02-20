e7fe022823292cb30898f8c249771e588459d0aa
samunu <micaolor@gmail.com>
2024-02-20 12:03:34 -0300

    empezando a controlar inputs de form login(en pañales)

--- Client/vite-project/src/components/login/Login.tsx
+++ Client/vite-project/src/components/login/Login.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
   return (
     <>
       <h1>Holi soy el Login</h1>
       <form>
         <label>Email:</label>
-        <input></input>
+        <input
+          type="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        ></input>
 
         <label>Password:</label>
-        <input></input>
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={handleChange}
+        ></input>
 
         <button>Log in</button>
 
