6227ea30a4e00b308c5762c75165b36f730d22b0
IfoliacoRamos <ivaleja3415@gmail.com>
2024-02-22 01:32:48 -0500

    no funciono el calendario, hay un error con las types pero aun renderiza la pagina

--- Client/vite-project/src/components/searchBar/SearchBar.tsx
+++ Client/vite-project/src/components/searchBar/SearchBar.tsx
@@ -1,10 +1,50 @@
 // framework Styles
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faLocationDot, faPerson } from '@fortawesome/free-solid-svg-icons';
+//import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+// dependencies
+import { useState } from "react";
 // Styles
 import styles from "../searchBar/SearchBar.module.css";
 
 const SearchBar: React.FC = () => {
+ 
+  // calendar framework
+ // interface State {
+ //   startDate: Date | undefined;
+ //   endDate: Date | undefined;
+ //   key: string;
+ // }
+  
+ // const initialState: State = {
+ //   startDate: new Date(),
+ //   endDate: undefined,
+ //   key: 'selection'
+ // };
+  
+
+ // const [state, setState] = useState<State[]>([initialState]);
+
+ // <DateRange
+ //     editableDateInputs={true}
+ //     onChange={item => setState([item.selection])}
+ //     moveRangeOnFirstSelection={false}
+ //     ranges={state}
+ //      />
+  interface Options {
+    people: number;
+    rooms: number;
+  }
+
+  const initialOptions: Options = {
+    people: 0,
+    rooms: 0
+  };
+
+  const [options, setOptions] = useState<Options[]>([initialOptions]);
+
   return (
     <div className={styles.headerSearch}>
     <div className={styles.headerSearchItem}>     
@@ -17,7 +57,7 @@ const SearchBar: React.FC = () => {
     </div>
     <div className={styles.headerSearchItem}>     
       <FontAwesomeIcon icon={faPerson} className={styles.headerIcon}/>
-      <span className={styles.headerSearchText}> # people # room </span>
+      <span className={styles.headerSearchText}> {`${options.people} people - ${options.rooms}`} </span>
     </div>
     <div className={styles.headerSearchItem}>     
       <button className={styles.headerBtn}> Search </button>
@@ -25,5 +65,6 @@ const SearchBar: React.FC = () => {
     </div>
   );
 };
+
 
 export default SearchBar;
