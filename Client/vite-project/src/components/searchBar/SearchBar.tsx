a7bf634fa2754ec19aff5f741114f4df05ff1cd2
IfoliacoRamos <ivaleja3415@gmail.com>
2024-02-21 23:26:23 -0500

    antes de los estilos

--- Client/vite-project/src/components/searchBar/SearchBar.tsx
+++ Client/vite-project/src/components/searchBar/SearchBar.tsx
@@ -1,8 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faLocationDot, faPerson } from '@fortawesome/free-solid-svg-icons';
+
 const SearchBar: React.FC = () => {
   return (
     <>
-      <h1>Holi soy el SearchBar</h1>
-      
+    <div className="">     
+      <FontAwesomeIcon icon={faLocationDot} className="" />
+      <input type="text" placeholder="Where we going?" className=""/>
+    </div>
+    <div className="">     
+      <FontAwesomeIcon icon={faCalendar} className=""/>
+      <span className=""> date to date </span>
+    </div>
+    <div className="">     
+      <FontAwesomeIcon icon={faPerson} className=""/>
+      <span className=""> 1 room </span>
+    </div>
     </>
   );
 };
