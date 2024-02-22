42caaed9affaa24ad7189cbb5e9796efefc59427
samunu <micaolor@gmail.com>
2024-02-21 23:39:54 -0300

    some changes we've done to cards

--- Client/vite-project/src/App.tsx
+++ Client/vite-project/src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-
+// ? -----------------------------------------------------COMPONENTS
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import LandingPage from "./components/landingPage/LandingPage";
 import NavBar from "./components/navBar/NavBar";
 import Favorites from "./components/favorites/Favorites";
-
+import Detail from "./components/detail/Detail";
+// ? -----------------------------------------------------STYLES
 import "./App.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </>
   );
--- Client/vite-project/src/components/card/Card.tsx
+++ Client/vite-project/src/components/card/Card.tsx
@@ -1,8 +1,33 @@
-const Card: React.FC = () => {
+import { Link } from "react-router-dom";
+
+interface CardC {
+  id?: string;
+  name: string;
+  address: string;
+  price: string;
+  images: ImageC[];
+}
+
+interface ImageC {
+  url: string;
+  alt: string;
+}
+
+const Card: React.FC<CardC> = (props: CardC) => {
   return (
-    <>
-      <h1>Holi soy Card</h1>
-    </>
+    <div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Hotel-room-renaissance-columbus-ohio.jpg/1280px-Hotel-room-renaissance-columbus-ohio.jpg"
+        alt=""
+      />
+      <h1>{props.name}Hotel name</h1>
+      <span> {props.address}address </span>
+      <span> breve descripcion solo es para ver</span>
+      <span> {props.price}price </span>
+      <Link to={`/detail/${props.id}`}>
+        <button> more.. </button>
+      </Link>
+    </div>
   );
 };
 
