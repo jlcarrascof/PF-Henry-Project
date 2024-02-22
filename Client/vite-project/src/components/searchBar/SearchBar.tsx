8e87f56891373223d882b5451cb456da1dd4d722
IfoliacoRamos <ivaleja3415@gmail.com>
2024-02-21 23:51:23 -0500

    avance con estilos basicos de la search

--- Client/vite-project/src/components/searchBar/SearchBar.tsx
+++ Client/vite-project/src/components/searchBar/SearchBar.tsx
@@ -1,18 +1,21 @@
+// framework Styles
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faLocationDot, faPerson } from '@fortawesome/free-solid-svg-icons';
+// Styles
+import styles from "../searchBar/SearchBar.module.css";
 
 const SearchBar: React.FC = () => {
   return (
     <>
-    <div className="">     
+    <div className={styles.headerSearch}>     
       <FontAwesomeIcon icon={faLocationDot} className="" />
       <input type="text" placeholder="Where we going?" className=""/>
     </div>
-    <div className="">     
+    <div className={styles.headerSearch}>     
       <FontAwesomeIcon icon={faCalendar} className=""/>
       <span className=""> date to date </span>
     </div>
-    <div className="">     
+    <div className={styles.headerSearch}>     
       <FontAwesomeIcon icon={faPerson} className=""/>
       <span className=""> 1 room </span>
     </div>
