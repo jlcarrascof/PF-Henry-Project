c68c8a6f1e3c593497cc417b5115e0fb0e93fda0
samunu <micaolor@gmail.com>
2024-02-23 05:11:10 -0300

    made some changes to design and create footer

--- Client/vite-project/src/components/footer/Footer.tsx
+++ Client/vite-project/src/components/footer/Footer.tsx
@@ -5,20 +5,20 @@ const Footer: React.FC = () => {
     <div className="footerContainer">
       <div className="marca">
         <h1>Rentify</h1>
-        <h3>
+        <h4>
           Welcome to the best app for discovering the best hotels for your
           holidays
-        </h3>
+        </h4>
       </div>
       <div className="comp">
-        <h5>Company</h5>
-        <h6>About us</h6>
-        <h6>What is Rentify?</h6>
+        <h4>Company</h4>
+        <h5>About us</h5>
+        <h5>What is Rentify?</h5>
       </div>
       <div className="pay">
-        <h5>Pay</h5>
-        <h6>Cards</h6>
-        <h6>Payment services</h6>
+        <h4>Pay</h4>
+        <h5>Cards</h5>
+        <h5>Payment services</h5>
       </div>
     </div>
   );
