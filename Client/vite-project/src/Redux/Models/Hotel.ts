c1f35b1ebbbd5a866eec875f0fd6b28981e58f8f
NazarethV <nazarethvillar38@gmail.com>
2024-02-24 16:13:53 -0300

    Arraglando

--- Client/vite-project/src/Redux/Models/Hotel.ts
+++
@@ -1,9 +0,0 @@
-// Define la estructura de un hotel
-export interface Hotel {
-    id: number;
-    name: string;
-    description: string;
-    location: string;
-    rating: number;
-    amenities: string[]; // Suponemos que las comodidades son cadenas
-  }
