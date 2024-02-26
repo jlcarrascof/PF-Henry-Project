81f9297d606322a91d2b1786ffc02aa0f1ff0972
miguel angel <miguelangelrodriguezpulgarin2@gmail.com>
2024-02-26 12:10:02 -0500

    (Miguel) maqueta Login buttons to home

--- Client/vite-project/src/components/login/Login.tsx
+++ Client/vite-project/src/components/login/Login.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { validation } from "./LogValidation";
-import "./Login.modules.css"
+import "./Login.modules.css";
 
 const Login: React.FC = () => {
   interface Data {
@@ -40,7 +40,7 @@ const Login: React.FC = () => {
           value={data.email}
           onChange={onChange}
           placeholder="myexample@gmail.com"
-        ></input>
+        />
         {errors.email && <p>{errors.email}</p>}
 
         <label>Password:</label>
@@ -49,15 +49,26 @@ const Login: React.FC = () => {
           name="password"
           value={data.password}
           onChange={onChange}
-          placeholder="enter yout password"
-        ></input>
+          placeholder="enter your password"
+        />
         {errors.password && <p>{errors.password}</p>}
 
-        <button type="submit">Log in</button>
+        {/* Cambios en los botones para redirigir a /home */}
+        <Link to="/home">
+          <button type="submit">Log in</button>
+        </Link>
 
-        <button>Continue with Google</button>
-        <button>Continue with Instagram</button>
-        <button>Continue with Facebook</button>
+        <Link to="/home">
+          <button>Continue with Google</button>
+        </Link>
+
+        <Link to="/home">
+          <button>Continue with Instagram</button>
+        </Link>
+
+        <Link to="/home">
+          <button>Continue with Facebook</button>
+        </Link>
 
         <Link to="/register">
           <span>Do not have an account? Sign in!</span>
