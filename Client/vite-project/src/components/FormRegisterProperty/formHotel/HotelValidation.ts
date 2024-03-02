4508649fee77a797ca896dce2991206b332c679a
samunu <micaolor@gmail.com>
2024-03-02 00:25:00 -0300

    finished file HotelValidation

--- Client/vite-project/src/components/FormRegisterProperty/formHotel/HotelValidation.ts
+++ Client/vite-project/src/components/FormRegisterProperty/formHotel/HotelValidation.ts
@@ -3,48 +3,49 @@ interface Error {
   details?: string;
   address?: string;
   contact?: {
-    phone?: number;
+    phone?: string;
     mail?: string;
   };
 }
 
 const hotelValidation = ({ name, details, address, contact }: Error) => {
-  let error: Error;
+  let error: Error = {};
   if (!name) {
     error.name = "This field cannot be blank";
-  }
-  if (name && !/^[A-Za-z]+$/.test(name)) {
+  } else if (name && !/^[A-Za-z]+$/.test(name)) {
     error.name = "Only letters are allowed";
-  }
-  if (name && name.length < 4) {
+  } else if (name && name.length < 4) {
     error.name = "That name is too short";
   }
 
   if (!details) {
     error.details = "This field cannot be blank";
-  }
-
-  if (details && details.length < 20) {
-    error.details = "The details must have at least 20 characters";
+  } else if (details && details.length < 25) {
+    error.details = "The details must have at least 25 characters";
   }
 
   if (!address) {
     error.address = "This field cannot be blank";
-  }
-  if (name && name.length < 4) {
-    error.name = "That address is too short";
+  } else if (name && name.length < 4) {
+    error.address = "That address is too short";
   }
 
   if (!contact?.phone) {
-    error.name = "This field cannot be blank";
+    error.contact?.phone = "This field cannot be blank";
+  } else if (
+    contact?.phone &&
+    !/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(
+      contact?.phone
+    )
+  ) {
+    error.contact?.phone = "That email is not valid";
   }
 
   if (!contact?.mail) {
     error.contact?.mail = "This field cannot be blank";
-  }
-  if (
+  } else if (
     contact?.mail &&
-    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(contact.mail)
+    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(contact?.mail)
   ) {
     error.contact?.mail = "That email is not valid";
   }
