12b0e091bac1e4799e809aff1dbfd4d10f1c7196
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-03-02 23:01:39 -0300

    estapa 1

--- Client/vite-project/src/components/login/Login.tsx
+++ Client/vite-project/src/components/login/Login.tsx
@@ -14,16 +14,53 @@ import {
 } from 'firebase/auth';
 import firebaseApp from './firebaseConfig';
 import { authenticateUser } from '../../Redux/Actions/actions';
-import "./Login.css";
-import Register from "../register/Register";
+import swal from 'sweetalert'
+import styled from 'styled-components'
 
 const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
 
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`;
+
+const ContenedorModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  z-index: 1001;
+`;
+
+const Encabezado = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ccc;
+`;
+
+const LoginButton = styled.button`
+  background-color: transparent;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+`;
 const Login: React.FC = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: any) => state.user);
   const [registration, setRegistration] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (userFirebase) => {
@@ -96,52 +133,60 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <div className="userFirebase">
-        <div className="padreFirebase">
-          <form onSubmit={firebaseAuthentication}>
-            <input type="text" placeholder="Enter your email" className="cajaTexto" id="email" />
-            <input type="password" placeholder="Enter your password" className="cajaTexto" id="password" />
-            <button>{registration ? "Sign up" : "Log in"}</button>
-          </form>
-          <div className="estilos-google">
-            <p> {registration ? "Already have an account?" : "Don't have an account?"}
-              <button onClick={() => setRegistration(!registration)}>{registration ? "Log in" : "Sign up"}</button>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="auth-status">
-        <div className="button-google">
-          <div className="card card-body">
-            {user ? (
-              <p>If you want to disconnect, click on <strong>Log out</strong></p>
-            ) : (
-              <p>You can also log in with your <strong>Google account</strong></p>
-            )}
-            {!user ? (
-              <button type="button" onClick={handleGoogleLogin}><img className="estilo-profile" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1709152706/login/qledtqlcwqfmqlh9zhe4.png" alt="Google logo" />Continue with Google</button>
-            ) : (
-              <div className="">
-                {user.providerId === "password" && (
-                  <button type="button" onClick={handleSignOut}>Log out</button>
-                )}
-                {user.providerId === "google.com" && (
-                  <button type="button" onClick={handleSignOut}><img className="estilo-profile" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1709152706/login/qledtqlcwqfmqlh9zhe4.png" alt="Google logo" />Log out</button>
-                )}
+      {showModal && (
+        <Overlay>
+          <ContenedorModal>
+            <Encabezado>
+              <LoginButton>Login</LoginButton>
+              <LoginButton onClick={() => setShowModal(false)}>x</LoginButton>
+            </Encabezado>
+            <div className="userFirebase">
+              <div className="padreFirebase">
+                <form onSubmit={firebaseAuthentication}>
+                  <input type="text" placeholder="Enter your email" className="cajaTexto" id="email" />
+                  <input type="password" placeholder="Enter your password" className="cajaTexto" id="password" />
+                  <button>{registration ? "Sign up" : "Log in"}</button>
+                </form>
+                <div className="estilos-google">
+                  <p> {registration ? "Already have an account?" : "Don't have an account?"}
+                    <button onClick={() => setRegistration(!registration)}>{registration ? "Log in" : "Sign up"}</button>
+                  </p>
+                </div>
               </div>
-            )}
-            {user && user.providerId === "password" && (
-              <p>You have successfully connected with the email: <b>{user.email}</b></p>
-            )}
-            {user && user.providerId === "google.com" && (
-              <p>User connected: <b>{user.displayName}</b></p>
-            )}
-          </div>
-        </div>
-      </div>
+            </div>
 
-      {user && <Register />}
+            <div className="auth-status">
+              <div className="button-google">
+                <div className="card card-body">
+                  {user ? (
+                    <p>If you want to disconnect, click on <strong>Log out</strong></p>
+                  ) : (
+                    <p>You can also log in with your <strong>Google account</strong></p>
+                  )}
+                  {!user ? (
+                    <button type="button" onClick={handleGoogleLogin}><img className="estilo-profile" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1709152706/login/qledtqlcwqfmqlh9zhe4.png" alt="Google logo" />Continue with Google</button>
+                  ) : (
+                    <div className="">
+                      {user.providerId === "password" && (
+                        <button type="button" onClick={handleSignOut}>Log out</button>
+                      )}
+                      {user.providerId === "google.com" && (
+                        <button type="button" onClick={handleSignOut}><img className="estilo-profile" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1709152706/login/qledtqlcwqfmqlh9zhe4.png" alt="Google logo" />Log out</button>
+                      )}
+                    </div>
+                  )}
+                  {user && user.providerId === "password" && (
+                    <p>You have successfully connected with the email: <b>{user.email}</b></p>
+                  )}
+                  {user && user.providerId === "google.com" && (
+                    <p>User connected: <b>{user.displayName}</b></p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </ContenedorModal>
+        </Overlay>
+      )}
     </>
   );
 };
