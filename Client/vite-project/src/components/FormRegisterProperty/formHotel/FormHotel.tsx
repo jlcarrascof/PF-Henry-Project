5823a563f04ca7fc845e376a70717a0fa63db182
samunu <micaolor@gmail.com>
2024-03-06 18:16:18 -0300

    hotel form with validations fixed

--- Client/vite-project/src/components/FormRegisterProperty/formHotel/FormHotel.tsx
+++ Client/vite-project/src/components/FormRegisterProperty/formHotel/FormHotel.tsx
@@ -1,21 +1,33 @@
 import { createHotels } from "../../../Redux/Actions/actions";
 import { useDispatch, Dispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import './FormHotel.css';
+import { hotelValidation } from "./HotelValidation.ts";
+import "./FormHotel.css";
 import Cloudinary from "../../cloudinary/Cloudinary.tsx";
-
-
-
 
 interface FormSchema {
   name: string;
   details: string;
   address: string;
-  images: string[]; 
+  images: string[];
   contact: {
     phone: number;
     mail: string;
   };
+}
+interface ErrorSchema {
+  name?: string;
+  details?: string;
+  address?: string;
+  images?: File[];
+  contact?: {
+    phone?: string;
+    mail?: string;
+  };
+}
+interface Values {
+  user_email: string;
+  message: string;
 }
 
 interface FormHotelProps {
@@ -29,11 +41,27 @@ const FormHotel: React.FC<FormHotelProps> = ({ setStepRegister }) => {
     name: "",
     details: "",
     images: [], // Cambiado a una matriz vacía
-    address: '',
+    address: "",
     contact: {
       phone: 0,
       mail: "",
     },
+  });
+
+  const [error, setError] = useState<ErrorSchema>({
+    name: "",
+    details: "",
+    images: [],
+    address: "",
+    contact: {
+      phone: "",
+      mail: "",
+    },
+  });
+
+  const [values, setValues] = useState<Values>({
+    user_email: "",
+    message: `Wohoo! It looks like you have posted a new hotel! Now it's public for people who wants to go on holidays in our app :D `,
   });
 
   useEffect(() => {
@@ -41,6 +69,8 @@ const FormHotel: React.FC<FormHotelProps> = ({ setStepRegister }) => {
     if (storageData !== null) {
       const parsedData = JSON.parse(storageData);
       setFormData(parsedData);
+      const formErrors = hotelValidation(formData);
+      setError(formErrors);
     }
   }, []);
 
@@ -48,15 +78,40 @@ const FormHotel: React.FC<FormHotelProps> = ({ setStepRegister }) => {
     window.localStorage.setItem("form-hoteldata", JSON.stringify(formData));
   }, [formData]);
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    isContact: boolean = false
   ) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-    window.localStorage.setItem("form-hoteldata", JSON.stringify(formData));
+    const { name, value } = event.target;
+    const updatedFormData = isContact
+      ? {
+          ...formData,
+          contact: {
+            ...formData.contact,
+            [name]: value,
+          },
+        }
+      : {
+          ...formData,
+          [name]: value,
+        };
+    setFormData(updatedFormData);
+    setError(hotelValidation(updatedFormData));
+    window.localStorage.setItem(
+      "form-hoteldata",
+      JSON.stringify(updatedFormData)
+    );
   };
+
+  // const handleInputChange = (
+  //   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) => {
+  //   setFormData({
+  //     ...formData,
+  //     [event.target.name]: event.target.value,
+  //   });
+  //   window.localStorage.setItem("form-hoteldata", JSON.stringify(formData));
+  // };
 
   const handleImageChange = (imageUrl: string) => {
     setFormData({
@@ -64,6 +119,18 @@ const FormHotel: React.FC<FormHotelProps> = ({ setStepRegister }) => {
       images: [...formData.images, imageUrl],
     });
     window.localStorage.setItem("form-hoteldata", JSON.stringify(formData));
+  };
+
+  const handleContactChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    handleChange(event, true);
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    handleChange(event);
   };
 
   const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
@@ -74,67 +141,87 @@ const FormHotel: React.FC<FormHotelProps> = ({ setStepRegister }) => {
       window.localStorage.removeItem("form-hoteldata");
       setStepRegister(2);
     } catch (error) {
-      console.error('Error in create Hotel: ', error);
+      console.error("Error in create Hotel: ", error);
     }
   };
 
   return (
-    <div className="form-hotels-container">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            required
-          />
-        </label>
-        <label>
-          Details:
-          <textarea
-            name="details"
-            value={formData.details}
-            onChange={handleInputChange}
-            required
-          />
-        </label>
-        <label>
-          Address:
-          <input
-            type="text"
-            name="address"
-            value={formData.address || ""}
-            onChange={handleInputChange}
-          />
-        </label>
-        <div>
+    <div className="allFormHotel">
+      <div className="form-hotels-container">
+        <h1>Post your hotel!</h1>
+        <form onSubmit={handleSubmit}>
           <label>
-            Email:
+            Name:
             <input
               type="text"
-              name="mail"
-              value={formData.contact.mail}
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          {error.name && <p>{error.name}</p>}
+          <label>
+            Details:
+            <textarea
+              name="details"
+              value={formData.details}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          {error.details && <p>{error.details}</p>}
+          <label>
+            Address:
+            <input
+              type="text"
+              name="address"
+              value={formData.address || ""}
               onChange={handleInputChange}
             />
           </label>
-          <label>
-            Phone:
-            <input
-              type="text"
-              name="phone"
-              value={formData.contact.phone }
-              onChange={handleInputChange}
-            />
-          </label>
-        </div>
+          {error.address && <p>{error.address}</p>}
+          <div>
+            <label>
+              Email:
+              <input
+                type="text"
+                name="mail"
+                value={formData.contact.mail}
+                onChange={handleContactChange}
+              />
+            </label>
+            {error.contact?.mail && <p>{error.contact?.mail}</p>}
+            <label>
+              Phone:
+              <input
+                type="text"
+                name="phone"
+                value={formData.contact.phone}
+                onChange={handleContactChange}
+              />
+            </label>
+            {error.contact?.phone && <p>{error.contact?.phone}</p>}
+          </div>
 
-        {/* Nuevo componente de Cloudinary */}
-        <Cloudinary onImageChange={handleImageChange} />
+          {/* Nuevo componente de Cloudinary */}
+          <Cloudinary onImageChange={handleImageChange} />
+          <input
+            className="messageInput"
+            name="user_email"
+            value={values.user_email}
+          ></input>
+          <input
+            className="messageInput"
+            name="message"
+            value={values.message}
+          ></input>
 
-        <button type="submit" className="formLogin button">Submit Hotel</button>
-      </form>
+          <button type="submit" className="formLogin button">
+            Submit Hotel
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
