83a9c8447dedd47865e44995095bd771745abf81
AgustinRacca6 <agusrac2014@gmail.com>
2024-03-08 18:41:23 -0300

    cambios liveshare

--- server/src/handlers/adminHandler.js
+++ server/src/handlers/adminHandler.js
@@ -1,6 +1,6 @@
 const { ObjectId } = require("mongodb");
 const { getDb } = require("../db");
-const { disableRoomId } = require("../controllers/adminController");
+const { disableRoomId, disableHotelId } = require("../controllers/adminController");
 
 
 const disableRoom = async (req, res) => {
@@ -38,7 +38,44 @@ const disableRoom = async (req, res) => {
   };
 
 
+  const disableHotel = async (req, res) => {
+    const {id} = req.params;
+    try {
+      if(!ObjectId.isValid(id)){
+        res.status(404).send("Room id not valid")
+        return
+      }
+      const result = await disableHotelId(id);
+
+      res.status(201).send(result)
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  }
+
+  const getDisabledHotels = async (req, res) => {
+    const db = getDb(); 
+    try {
+      const totalResults = await db.collection("hotels").countDocuments();
+  
+      const hotels = await db
+        .collection("hotels")
+        .find()
+        .toArray();
+  
+      res.status(200).json({
+        totalResults: totalResults,
+        hotels: hotels,
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+
+
   module.exports = {
     disableRoom,
-    getDisabledRooms
+    getDisabledRooms,
+    disableHotel,
+    getDisabledHotels
   }
--- server/src/routes/adminRouter.js
+++ server/src/routes/adminRouter.js
@@ -1,11 +1,15 @@
 const { Router } = require("express")
-const { getDisabledRooms, disableRoom } = require("../handlers/adminHandler")
+const { getDisabledRooms, disableRoom, getDisabledHotels, disableHotel } = require("../handlers/adminHandler")
 
 const adminRouter = Router()
 
 adminRouter.get("/rooms/", getDisabledRooms)
 
 adminRouter.patch("/rooms/:id", disableRoom)
+
+adminRouter.get("/hotels/", getDisabledHotels)
+
+adminRouter.patch("/hotels/:id", disableHotel)
 
 
 
