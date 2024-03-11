7a2fd71fa0db62483708a547587ba5fe30e124c0
IfoliacoRamos <ivaleja3415@gmail.com>
2024-03-10 19:42:24 -0500

    index de rutas trasladado

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -12,3 +12,22 @@ mainRouter.use('/hotels', hotelsRouter);
 mainRouter.use('/admin', adminRouter)
 
 module.exports = mainRouter;
+
+//! EL QUE TENIA EN LA OTRA RAMA DE PAGO
+
+/* const { Router } = require('express');
+const usersRouter = require('./usersRouter');
+const hotelsRouter = require('./hotelsRouter');
+const roomsRouter = require('./roomRouter');
+const favRouter = require('./favRouter');
+const adminRouter = require('./adminRouter');
+const mercadoRouter = require('./mercadoPago.routes');
+
+
+mainRouter.use('/favs', favRouter)
+mainRouter.use('/rooms', roomsRouter); 
+mainRouter.use('/users', usersRouter);  
+mainRouter.use('/hotels', hotelsRouter); 
+mainRouter.use('/admin', adminRouter);
+mainRouter.use('/payment', mercadoRouter); */
+
