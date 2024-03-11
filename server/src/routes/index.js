ce3e34b34e32b6fa37af275dd4b97075df526580
IfoliacoRamos <ivaleja3415@gmail.com>
2024-03-10 20:31:07 -0500

    index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ const usersRouter = require('./usersRouter');
 const hotelsRouter = require('./hotelsRouter');
 const roomsRouter = require('./roomRouter');
 const adminRouter = require('./adminRouter');
+const mercadoRouter = require('./mercadoPago.routes');
 
 const mainRouter = Router();
 
@@ -10,24 +11,8 @@ mainRouter.use('/rooms', roomsRouter);
 mainRouter.use('/users', usersRouter);  
 mainRouter.use('/hotels', hotelsRouter); 
 mainRouter.use('/admin', adminRouter)
+mainRouter.use('/payment', mercadoRouter);
 
 module.exports = mainRouter;
 
-//! EL QUE TENIA EN LA OTRA RAMA DE PAGO
-
-/* const { Router } = require('express');
-const usersRouter = require('./usersRouter');
-const hotelsRouter = require('./hotelsRouter');
-const roomsRouter = require('./roomRouter');
-const favRouter = require('./favRouter');
-const adminRouter = require('./adminRouter');
-const mercadoRouter = require('./mercadoPago.routes');
-
-
-mainRouter.use('/favs', favRouter)
-mainRouter.use('/rooms', roomsRouter); 
-mainRouter.use('/users', usersRouter);  
-mainRouter.use('/hotels', hotelsRouter); 
-mainRouter.use('/admin', adminRouter);
-mainRouter.use('/payment', mercadoRouter); */
 
