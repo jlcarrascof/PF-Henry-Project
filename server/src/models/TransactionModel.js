2b6decc82680847ef53b5c4d3df3482b6e5f8109
IfoliacoRamos <ivaleja3415@gmail.com>
2024-03-10 19:38:46 -0500

    cambie el modelo

--- server/src/models/TransactionModel.js
+++ server/src/models/TransactionModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/* const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const TransactionSchema = new Schema({
@@ -23,6 +23,41 @@ const TransactionSchema = new Schema({
   installments: Number,
 });
 
-const Transaction = model("Transaction", TransactionSchema);
+const Transaction = model("Transaction", TransactionSchema); */
 
+//! AQUI CAMBIE EL MODELO DE TRANSACCION, COMENTE EL VIEJO ARRIBA
 module.exports = Transaction;
+
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const TransactionSchema = new Schema({
+  paymentId: { type: String, required: true },
+  /* payerName: { type: String, required: true },
+  payerEmail: {
+    type: String,
+    required: true,
+  }, */
+  status: {
+    type: String,
+    enum: ["approved", "refunded", /* "charged_back" */],
+  },
+  payment_type: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      id: { type: Schema.Types.ObjectId, ref: "Room", required: true },
+      currency: { type: String, required: true },
+      description: { type: String, required: true },
+      quantity: { type: Number, required: true, min: 1 },
+      unitPrice: { type: Number, required: true },
+    },
+  ],
+  installments: Number,
+});
+
+
+const Transaction = model("Transaction", TransactionSchema);
