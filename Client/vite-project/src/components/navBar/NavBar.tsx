3b8954e5d8505b2d581e1eaf53e85ba6381d1cf5
samunu <micaolor@gmail.com>
2024-03-13 11:36:56 -0300

    algunos cambios de fede

--- Client/vite-project/src/components/navBar/NavBar.tsx
+++ Client/vite-project/src/components/navBar/NavBar.tsx
@@ -12,63 +12,122 @@ export const NavBar: React.FC = () => {
   // let user = window.localStorage.getItem("user");
 
   return (
-    <div className="navBar">
-      <div className="navContainer">
+    <header>
+      <div className="headerTop">
         <Link to="/">
-          <h2 className="logo"> Rentify </h2>
+          <div className="headerTopLeft">
+            <h2 className="logo"> Rentify </h2>
+          </div>
+          <div className="headerTopRight">
+            {user === null || Object.keys(user).length === 0 ? (
+              <>
+                <Link className="btnLogin" to="/login">
+                  <p>Login</p>
+                </Link>
+              </>
+            ) : (
+              <>
+                <div className="dropbox">
+                  <div className="avatar">
+                    {user.Message}
+                    {user && (
+                      <Avatar
+                        alt=""
+                        src={user.image}
+                        size={48}
+                        gap={25}
+                        draggable="false"
+                      />
+                    )}
+                  </div>
+                  <LongMenu />
+                </div>
+              </>
+            )}
+          </div>
         </Link>
-        <div className="navSeparator"></div>
-        <div className="navItems">
-          <Link to="/admin">
-            <p>Admin</p>
-          </Link>
-          <Link to="/">
-            <p>Home</p>
-          </Link>
-          <Link to="/rooms">
-            <p>Search for rooms</p>
-          </Link>
-          <Link to="/register-hotel">
-            <p>Post a hotel</p>
-          </Link>
-          {/* <Link to="/reservations">
+      </div>
+      <div className="headerBottom">
+        <Link to="/admin">
+          <p>Admin</p>
+        </Link>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/rooms">
+          <p>Search for rooms</p>
+        </Link>
+        <Link to="/register-hotel">
+          <p>Post a hotel</p>
+        </Link>
+        {/* <Link to="/reservations">
             <p>Cart Reservation</p>
           </Link> */}
-          <Link to="/favorites">
-            <p>Favorites</p>
-          </Link>
-          <Link to="/my-reservations">
-            <p>My Reservations</p>
-          </Link>
-          {user === null || Object.keys(user).length === 0 ? (
-            <>
-              <Link className="btnLogin" to="/login">
-                <p>Login</p>
-              </Link>
-            </>
-          ) : (
-            <>
-              <div className="dropbox">
-                <div className="avatar">
-                  {user.Message}
-                  {user && (
-                    <Avatar
-                      alt=""
-                      src={user.image}
-                      size={48}
-                      gap={25}
-                      draggable="false"
-                    />
-                  )}
-                </div>
-                <LongMenu />
-              </div>
-            </>
-          )}
-        </div>
+        <Link to="/favorites">
+          <p>Favorites</p>
+        </Link>
+        <Link to="/my-reservations">
+          <p>My Reservations</p>
+        </Link>
       </div>
-      {/* {showLogin && <Login />} */}
-    </div>
+    </header>
+    // <div className="navBar">
+    //   <div className="navContainer">
+    //     <Link to="/">
+    //       <h2 className="logo"> Rentify </h2>
+    //     </Link>
+    //     <div className="navSeparator"></div>
+    //     <div className="navItems">
+    // <Link to="/admin">
+    //   <p>Admin</p>
+    // </Link>
+    // <Link to="/">
+    //   <p>Home</p>
+    // </Link>
+    // <Link to="/rooms">
+    //   <p>Search for rooms</p>
+    // </Link>
+    // <Link to="/register-hotel">
+    //   <p>Post a hotel</p>
+    // </Link>
+    // {/* <Link to="/reservations">
+    //   <p>Cart Reservation</p>
+    // </Link> */}
+    // <Link to="/favorites">
+    //   <p>Favorites</p>
+    // </Link>
+    // <Link to="/my-reservations">
+    //   <p>My Reservations</p>
+    // </Link>
+    // {user === null || Object.keys(user).length === 0 ? (
+    //   <>
+    //     <Link className="btnLogin" to="/login">
+    //       <p>Login</p>
+    //     </Link>
+    //   </>
+    // ) : (
+    //   <>
+    //     <div className="dropbox">
+    //       <div className="avatar">
+    //         {user.Message}
+    //         {user && (
+    //           <Avatar
+    //             alt=""
+    //             src={user.image}
+    //             size={48}
+    //             gap={25}
+    //             draggable="false"
+    //           />
+    //         )}
+    //       </div>
+    //       <LongMenu />
+    //     </div>
+    //   </>
+    // )}
+    //     </div>
+    //   </div>
+    //   {/* {showLogin && <Login />} */}
+    // </div>
   );
 };
 
